6586a25f54047bac2b3253766334ab61356824ca
melbo <40171076+melbo911@users.noreply.github.com>
2020-10-19 20:09:36 +0200

    Delete init.sh

--- init.sh
+++
@@ -1,9 +0,0 @@
-echo "# xroads" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/melbo911/xroads.git
-git push -u origin main
-
-
