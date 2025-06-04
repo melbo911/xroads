901224dadb9a14f13ddc03eb333ca4f10139b866
Melbo <melbo@jnet.ath.cx>
2025-06-04 12:55:46 +0200

    changes

--- src/c/xroads.bat
+++ src/c/xroads.bat
@@ -1,6 +1,8 @@
 @echo off
 
-rem show xroads cmd line options
+rem stands for "remark" or comment to show examples !!
+
+rem show xroads help and cmd line options
 rem xroads.exe -h
 
 rem create log file from output for debugging
@@ -12,8 +14,17 @@ rem xroads.exe -v 80
 rem enable left-hand-driving support 
 rem xroads.exe -l
 
-rem also hide rail tracks
+rem hide rail tracks
 rem xroads.exe -r
+
+rem hide lights on primary and secondary streets 
+rem xroads.exe -s
+
+rem print additional debug information
+rem xroads.exe -d
+
+rem example of how to combine multiple options
+rem xroads.exe -v 80 -r -s
 
 rem use xroads default settings
 xroads.exe
